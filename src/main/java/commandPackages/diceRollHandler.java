f22e981f5bbfc1d84a4eea4c393d641bf5403b9d
Josef Baltz <josef@baltz.net>
2018-02-26 10:23:28 -0600

    Updated Dice Handler
    Mainly Passes
    TODO Test Further Dice Roll Checks
    TODO Setup JSON requests to Random.org

--- src/main/java/commandPackages/diceRollHandler.java
+++ src/main/java/commandPackages/diceRollHandler.java
@@ -21,38 +21,49 @@ public class diceRollHandler {
             case "roll":
                 rollInit(event, argsList);
                 System.out.println("[INFO] roll command received");
-                System.out.println("    Command sent by" + event.getAuthor().getDisplayName(event.getGuild()));
+                System.out.println("    Command sent by " + event.getAuthor().getDisplayName(event.getGuild()));
         }
     }
     private void rollInit(MessageReceivedEvent event, List<String> args) {
-        if (args.isEmpty() || args.get(1).isEmpty()) {
-            coreUtils.sendMessage(event.getChannel(), "Missing Arguments Received!");
+        if (args.isEmpty() || args.size() <=1 ) {
+            coreUtils.sendMessage(event.getChannel(), "Not Enough Arguments Received!");
             coreUtils.sendMessage(event.getChannel(), "    ```" + coreUtils.BOT_PREFIX + "roll {rolls 1-4} {dice}```");
         } else if (args.size() >= 3) {
             coreUtils.sendMessage(event.getChannel(), "Too Many Arguments Received!");
             coreUtils.sendMessage(event.getChannel(), "    ```" + coreUtils.BOT_PREFIX + "roll {rolls 1-4} {dice}```");
         } else if (args.get(0).equals("1")){
-            randomorgCall(1, args.get(1), event);
+            randomorgCall(1, args, event);
 
         } else if (args.get(0).equals("2")){
-            randomorgCall(2, args.get(1), event);
+            randomorgCall(2, args, event);
 
         } else if (args.get(0).equals("3")){
-            randomorgCall(3, args.get(1), event);
+            randomorgCall(3, args, event);
 
         } else if (args.get(0).equals("4")){
-            randomorgCall(4, args.get(1), event);
+            randomorgCall(4, args, event);
 
-        } else { coreUtils.sendMessage(event.getChannel(), "```css [Unknown Error]```"); }
+        } else { coreUtils.sendMessage(event.getChannel(), "```[Unknown Error]```"); }
     }
-    private void randomorgCall(int i, String die, MessageReceivedEvent event){
-        if (!die.equals("d2") || !die.equals("d4") ||
-                !die.equals("d6") || !die.equals("d8") ||
-                !die.equals("d10") || !die.equals("d12") ||
-                !die.equals("d20") || !die.equals("d100")){
-            coreUtils.sendMessage(event.getChannel(), "Missing Die Type!");
-        } else {
-            coreUtils.sendMessage(event.getChannel(), "Checks Passed!");
+    private void randomorgCall(int i, List<String> args, MessageReceivedEvent event){
+        switch (args.get(1)) {
+            case "d2":
+                coreUtils.sendMessage(event.getChannel(), "d2 Received");
+            case "d4":
+                coreUtils.sendMessage(event.getChannel(), "d4 Received");
+            case "d6":
+                coreUtils.sendMessage(event.getChannel(), "d6 Received");
+            case "d8":
+                coreUtils.sendMessage(event.getChannel(), "d8 Received");
+            case "d10":
+                coreUtils.sendMessage(event.getChannel(), "d10 Received");
+            case "d12":
+                coreUtils.sendMessage(event.getChannel(), "d12 Received");
+            case "d20":
+                coreUtils.sendMessage(event.getChannel(), "d20 Received");
+            case "d100":
+                coreUtils.sendMessage(event.getChannel(), "d100 Received");
         }
+        coreUtils.sendMessage(event.getChannel(), "Improper Dice Type!");
     }
 }
