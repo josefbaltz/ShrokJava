318fae1fc5dc519f4ed7fbcac9cb2927beda57a5
Josef Baltz <josef@baltz.net>
2018-06-08 15:42:36 -0500

    v1.2.2.3
    - Updated Roll Command to allow for dice modifications such as ]roll 3 d20 +3

--- src/main/java/commandPackages/rollCommand/diceRollHandler.java
+++ src/main/java/commandPackages/rollCommand/diceRollHandler.java
@@ -28,10 +28,10 @@ public class diceRollHandler {
                 try {
                     Integer.parseInt(argsList.get(1).substring(1));
                 } catch (IllegalArgumentException e) {
-                    sendHelp(event.getChannel(), "]roll {1-5} d{Dice Type}", "]roll 5 d20");
+                    sendHelp(event.getChannel(), "]roll {1-5} d{Dice Type} (+/-#)", "]roll 5 d20 +6");
                     throw e;
                 } catch (IndexOutOfBoundsException e){
-                    sendHelp(event.getChannel(), "]roll {1-5} d{Dice Type}", "]roll 5 d20");
+                    sendHelp(event.getChannel(), "]roll {1-5} d{Dice Type} (+/-#)", "]roll 5 d20 +6");
                     throw e;
                 }
                 rollInit(event, argsList);
@@ -41,20 +41,75 @@ public class diceRollHandler {
         }
     }
     private void rollInit(MessageReceivedEvent event, List<String> args) throws InterruptedException {
-        if (args.isEmpty() || args.size() <=1 || args.size() >= 3 || Integer.parseInt(args.get(0)) >= 6 ||
+        if (args.isEmpty() || args.size() <=1 || args.size() >= 4 || Integer.parseInt(args.get(0)) >= 6 ||
                 Integer.parseInt(args.get(0)) <= 0) {
-            sendHelp(event.getChannel(), "]roll {1-5} d{Dice Type}", "]roll 5 d20");
+            sendHelp(event.getChannel(), "]roll {1-5} d{Dice Type} (+/-#)", "]roll 5 d20 +6");
         } else {
-                dieCall(Integer.parseInt(args.get(0)), args, event);
+            dieCall(Integer.parseInt(args.get(0)), args, event);
         }
     }
     private void dieCall(int i, List<String> args, MessageReceivedEvent event) throws InterruptedException {
         EmbedBuilder builder = new EmbedBuilder();
         builder.withTitle(event.getAuthor().getDisplayName(event.getGuild()) + " Roll Results");
         builder.withColor(20, 60, 255);
-        for (int x = 0; x <= Integer.parseInt(args.get(0)) - 1; x++) {
-            int dieSize = Integer.parseInt(args.get(1).substring(1));
-            builder.appendField("Roll", Integer.toString(randomService.randomNumber(dieSize, event)), true);
+        ArrayList<Integer> RandomList = new ArrayList<Integer>();
+        try {
+            if (args.get(2).startsWith("+")) {
+                for (int x = 0; x <= Integer.parseInt(args.get(0)) - 1; x++) {
+                    int dieSize = Integer.parseInt(args.get(1).substring(1));
+                    RandomList.add(randomService.randomNumber(dieSize, event));
+                }
+                switch (Integer.parseInt(args.get(0))) {
+                    case 1:
+                        builder.appendField("Roll", Integer.toString(RandomList.get(0) + Integer.parseInt(args.get(2).replace("+", ""))), true);
+                        break;
+                    case 2:
+                        builder.appendField("Roll", Integer.toString((RandomList.get(0) + RandomList.get(1)) + Integer.parseInt(args.get(2).replace("+", ""))), true);
+                        break;
+                    case 3:
+                        builder.appendField("Roll", Integer.toString((RandomList.get(0) + RandomList.get(1) + RandomList.get(2)) + Integer.parseInt(args.get(2).replace("+", ""))), true);
+                        break;
+                    case 4:
+                        builder.appendField("Roll", Integer.toString((RandomList.get(0) + RandomList.get(1) + RandomList.get(2) + RandomList.get(3)) + Integer.parseInt(args.get(2).replace("+", ""))), true);
+                        break;
+                    case 5:
+                        builder.appendField("Roll", Integer.toString((RandomList.get(0) + RandomList.get(1) + RandomList.get(2) + RandomList.get(3) + RandomList.get(4)) + Integer.parseInt(args.get(2).replace("+", ""))), true);
+                        break;
+                    default:
+                        sendHelp(event.getChannel(), "]roll {1-5} d{Dice Type} (+/-#)", "]roll 5 d20 +6");
+                        break;
+                }
+            } else if (args.get(2).startsWith("-")) {
+                for (int x = 0; x <= Integer.parseInt(args.get(0)) - 1; x++) {
+                    int dieSize = Integer.parseInt(args.get(1).substring(1));
+                    RandomList.add(randomService.randomNumber(dieSize, event));
+                }
+                switch (Integer.parseInt(args.get(0))) {
+                    case 1:
+                        builder.appendField("Roll", Integer.toString(RandomList.get(0) - Integer.parseInt(args.get(2).replace("-", ""))), true);
+                        break;
+                    case 2:
+                        builder.appendField("Roll", Integer.toString((RandomList.get(0) + RandomList.get(1)) - Integer.parseInt(args.get(2).replace("-", ""))), true);
+                        break;
+                    case 3:
+                        builder.appendField("Roll", Integer.toString((RandomList.get(0) + RandomList.get(1) + RandomList.get(2)) - Integer.parseInt(args.get(2).replace("-", ""))), true);
+                        break;
+                    case 4:
+                        builder.appendField("Roll", Integer.toString((RandomList.get(0) + RandomList.get(1) + RandomList.get(2) + RandomList.get(3)) - Integer.parseInt(args.get(2).replace("-", ""))), true);
+                        break;
+                    case 5:
+                        builder.appendField("Roll", Integer.toString((RandomList.get(0) + RandomList.get(1) + RandomList.get(2) + RandomList.get(3) + RandomList.get(4)) - Integer.parseInt(args.get(2).replace("-", ""))), true);
+                        break;
+                    default:
+                        sendHelp(event.getChannel(), "]roll {1-5} d{Dice Type} (+/-#)", "]roll 5 d20 +6");
+                        break;
+                }
+            }
+        } catch (java.lang.IndexOutOfBoundsException e) {
+            for (int x = 0; x <= Integer.parseInt(args.get(0)) - 1; x++) {
+                int dieSize = Integer.parseInt(args.get(1).substring(1));
+                builder.appendField("Roll", Integer.toString(randomService.randomNumber(dieSize, event)), true);
+            }
         }
         builder.withFooterText("ShrokBot (c) 2018");
         RequestBuffer.request(() -> {
